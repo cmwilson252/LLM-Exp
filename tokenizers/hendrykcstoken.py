441d651f6670cd740f9a6473b43f9c4f8b99a3db
cmwilson252 <wilsoncolorado12@gmail.com>
2023-08-06 11:02:55 -0500

    errors

--- tokenizers/hendrykcstoken.py
+++ tokenizers/hendrykcstoken.py
@@ -4,8 +4,8 @@ import torch
 
 # Load the data from the spreadsheet
 file_path = "/cow02/rudenko/colowils/LLMExp/LLM-Exp/eval/data/college_chemistry_test.csv"
-data = pd.read_csv(file_path
-data_array = data.values)
+data = pd.read_csv(file_path)
+data_array = data.values
 questions = data_array[:, 0].tolist()
 choices = data_array[:, 1:5].tolist()
 correct_answers = data_array[:, 5].tolist()
@@ -16,7 +16,7 @@ tokenizer = AutoTokenizer.from_pretrained('/cow02/rudenko/colowils/LLMExp/Llama-
 # Tokenize the questions and choices
 tokenized_data = []
 for question, choices_list in zip(questions, choices):
-    encoded = tokenizer(question, choices_list, return_tensors="pt", padding=True, truncation=True)
+    encoded = tokenizer(question, choices_list, return_tensors="pt", padding=False, truncation=False)
     tokenized_data.append(encoded)
 
 # Save the tokenized data to a file
