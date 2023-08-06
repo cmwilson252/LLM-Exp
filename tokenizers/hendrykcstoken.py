0225c9ab39c604cf93662271e7a2fb795c963512
cmwilson252 <wilsoncolorado12@gmail.com>
2023-08-06 11:22:13 -0500

    hendrycks college_chem tokenizer

--- tokenizers/hendrykcstoken.py
+++ tokenizers/hendrykcstoken.py
@@ -16,9 +16,9 @@ tokenizer = AutoTokenizer.from_pretrained('/cow02/rudenko/colowils/LLMExp/Llama-
 tokenized_data = []
 for question, choice_list in zip(questions, choices):
     # Combine the question with each choice and tokenize
-    tokenized_choices = [tokenizer.encode_plus(question, choice, return_tensors="pt", padding=True, truncation=True) for choice in choice_list]
+    tokenized_choices = [tokenizer.encode_plus(question, choice, return_tensors="pt", padding=False, truncation=False) for choice in choice_list]
     tokenized_data.append(tokenized_choices)
 
 # Save the tokenized data to a file
-save_path = "/cow02/rudenko/colowils/LLMExp/"
+save_path = "/cow02/rudenko/colowils/LLMExp/hbio.pt"
 torch.save(tokenized_data, save_path)
