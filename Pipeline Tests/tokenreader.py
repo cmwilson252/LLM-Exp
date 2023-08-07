c116722dcd9250eafadcf69184c1b8ad3c69b9ae
Colorado <44687779+cmwilson252@users.noreply.github.com>
2023-08-07 10:42:04 -0500

    Update tokenreader.py

--- Pipeline Tests/tokenreader.py
+++ Pipeline Tests/tokenreader.py
@@ -6,19 +6,21 @@ from transformers import AutoTokenizer
 data_csv_path = "/cow02/rudenko/colowils/LLMExp/LLM-Exp/eval/data/college_chemistry_test.csv"
 data_df = pd.read_csv(data_csv_path, header=None)
 
-# Extract the correct answers as column indices (2, 3, 4, 5)
-correct_answers = data_df[5].tolist()
-
 # Load the tokenized data
 tokenized_data = torch.load("/cow02/rudenko/colowils/LLMExp/LLM-Exp/tokenizers/tokenized_data/hchem.pt")
+
+# Read the correct answers as text
+correct_answers_text = data_df[5].tolist()
 
 # Print the first 3 questions and their related answers + correct answer
 for i in range(3):
     question_data = tokenized_data[i]
     question_text = tokenizer.decode(question_data['input_ids'][0])
     answers_text = [tokenizer.decode(choice) for choice in question_data['input_ids'][1:]]
-    correct_answer_index = correct_answers[i] - 2  # Adjusting the index to 0-based
-    correct_answer_text = answers_text[correct_answer_index]
+    correct_answer_text = correct_answers_text[i]
+
+    # Find the index of the correct answer based on the text
+    correct_answer_index = answers_text.index(correct_answer_text)
 
     print(f"Question {i + 1}: {question_text}")
     print("Answer Choices:")
