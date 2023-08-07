39523a31b36e8d24d5be7c3c3d7e67b655152d3b
Colorado <44687779+cmwilson252@users.noreply.github.com>
2023-08-07 10:36:23 -0500

    Update tokenreader.py

--- Pipeline Tests/tokenreader.py
+++ Pipeline Tests/tokenreader.py
@@ -1,26 +1,27 @@
+import pandas as pd
 import torch
 from transformers import AutoTokenizer
-import pandas as pd 
 
+# Read the CSV file
 data_csv_path = "/cow02/rudenko/colowils/LLMExp/LLM-Exp/eval/data/college_chemistry_test.csv"
 data_df = pd.read_csv(data_csv_path, header=None)
 
-tokenized_data = torch.load("/cow02/rudenko/colowils/LLMExp/LLM-Exp/tokenizers/tokenized_data/hchem.pt")
-
+# Extract the correct answers as column indices (2, 3, 4, 5)
 correct_answers = data_df[5].tolist()
 
-# Load the tokenizer
-tokenizer = AutoTokenizer.from_pretrained('/cow02/rudenko/colowils/LLMExp/Llama-2-7b-chat-hf')
+# Load the tokenized data
+tokenized_data = torch.load("/cow02/rudenko/colowils/LLMExp/LLM-Exp/tokenizers/tokenized_data/hchem.pt")
 
+# Print the first 3 questions and their related answers + correct answer
 for i in range(3):
-        question_data = tokenized_data[i]
-        question_text = tokenizer.decode(question_data["input_ids"][0])
-        answers_text = [tokenizer.decode(choice) for choice in question_data["input_ids"][1:]]
-        correct_answer_index = correct_answers[i] - 2
-        correct_answer_text = answers_text[correct_answer_index]
+    question_data = tokenized_data[i]
+    question_text = tokenizer.decode(question_data['input_ids'][0])
+    answers_text = [tokenizer.decode(choice) for choice in question_data['input_ids'][1:]]
+    correct_answer_index = correct_answers[i] - 2  # Adjusting the index to 0-based
+    correct_answer_text = answers_text[correct_answer_index]
 
-        print(f"Question {i + 1}: {question_text}")
-        print("Answer choices:")
-        for j, answer in enumerate(answers_text):
-            print(f"{chr(97 + j)}. {answer}")
-        print(f"Correct Answer: {chr(97 + correct_answer_index)}. {correct_answer_text}\n")
+    print(f"Question {i + 1}: {question_text}")
+    print("Answer Choices:")
+    for j, answer in enumerate(answers_text):
+        print(f"{chr(97 + j)}. {answer}")
+    print(f"Correct Answer: {chr(97 + correct_answer_index)}. {correct_answer_text}\n")
