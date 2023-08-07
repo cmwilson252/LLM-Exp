2c3bd00784b8e8294311a39e5910d5f84a5a5ba1
Colorado <44687779+cmwilson252@users.noreply.github.com>
2023-08-07 11:02:56 -0500

    Update tokenreader.py

--- Pipeline Tests/tokenreader.py
+++ Pipeline Tests/tokenreader.py
@@ -18,18 +18,20 @@ tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 # Read the correct answers as text
 correct_answer_letters = data_df[5].tolist()
 
-# Print the first 3 questions and their related answers + correct answer
+# Print the first 3 questions
 for i in range(3):
     question_data = tokenized_data[i]
     question_text = tokenizer.decode(question_data['input_ids'][0])
-    answers_text = [tokenizer.decode(choice) for choice in question_data['input_ids'][1:]]
+    answers_text = [tokenizer.decode(choice) for choice in question_data['input_ids'][1:5]]  # Limit to 4 answer choices
     correct_answer_letter = correct_answer_letters[i]
-    
-    # Convert the correct answer letter to an index (0 for "A", 1 for "B", etc.)
     correct_answer_index = ord(correct_answer_letter) - ord('A')
 
-    print(f"Question {i + 1}: {question_text}")
-    print("Answer Choices:")
-    for j, answer in enumerate(answers_text):
-        print(f"{chr(97 + j)}. {answer}")
-    print(f"Correct Answer: {chr(97 + correct_answer_index)}. {answers_text[correct_answer_index]}\n")
+    # Check if the index is within the range of the answers
+    if 0 <= correct_answer_index < len(answers_text):
+        print(f"Question {i + 1}: {question_text}")
+        print("Answer Choices:")
+        for j, answer in enumerate(answers_text):
+            print(f"{chr(97 + j)}. {answer}")
+        print(f"Correct Answer: {chr(97 + correct_answer_index)}. {answers_text[correct_answer_index]}\n")
+    else:
+        print(f"Error with question {i + 1}: Correct answer index is out of range.\n")
