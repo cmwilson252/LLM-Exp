83905d4b231b745f569fcffc3affffef57db6e4d
Colorado <44687779+cmwilson252@users.noreply.github.com>
2023-08-07 11:59:34 -0500

    Update hendrykcstoken.py

--- tokenizers/hendrykcstoken.py
+++ tokenizers/hendrykcstoken.py
@@ -12,15 +12,22 @@ choices = data.iloc[:, 1:4].tolist()  # Adjusted to include only columns 1 to 4 
 tokenizer_model_path = '/cow02/rudenko/colowils/LLMExp/Llama-2-7b-chat-hf'
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_model_path)
 
-# Tokenize the questions and choices with padding
+# Calculate the maximum length among all combined texts
+max_length = max(len(tokenizer.tokenize(text)) for question, choices_list in zip(questions, choices)
+                 for text in [question + " " + str(choice) for choice in choices_list])
+
+# Tokenize the questions and choices with padding to the calculated maximum length
 tokenized_data_adjusted = []
 for question, choices_list in zip(questions, choices):
-    # Combine the question with each choice and tokenize
-    combined_texts = [question + " " + str(choice) for choice in choices_list]
-    encoded_data = tokenizer(combined_texts, padding='max_length', truncation=True)
+    # Tokenize each choice combined with the question
+    encoded_data_choices = []
+    for choice in choices_list:
+        combined_text = question + " " + str(choice)
+        encoded_data = tokenizer([combined_text], padding='max_length', max_length=max_length, truncation=False)
+        encoded_data_choices.append(encoded_data)
     
     # Append the tokenized data for this question
-    tokenized_data_adjusted.append(encoded_data)
+    tokenized_data_adjusted.append(encoded_data_choices)
 
 # Save the tokenized data to a file
 save_path = "/cow02/rudenko/colowils/LLMExp/LLM-Exp/tokenizers/tokenized_data/hchem.pt"
