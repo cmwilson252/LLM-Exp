1a5d0627501413e72b8501e6205843259145f460
Colorado <44687779+cmwilson252@users.noreply.github.com>
2023-08-13 12:39:58 -0500

    Update traintest.py

--- Pipeline Tests/traintest.py
+++ Pipeline Tests/traintest.py
@@ -7,59 +7,44 @@ from transformers import AutoTokenizer
 
 wandb.init(project="traintest")
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-tokenizer = AutoTokenizer.from_pretrained("/cow02/rudenko/colowils/LLMExp/Llama-2-7b-chat-hf")
-
-class TokenizedDataset(Dataset):
-    def __init__(self, file_path, tokenizer):
-        # Load pt file
-        self.data = torch.load(file_path)
+class MinimalDataset(Dataset):
+    def __init__(self, tokenizer):
         self.tokenizer = tokenizer
+        self.texts = ["A"]  # Just one token
+        self.labels = [0]   # Corresponding label
 
     def __len__(self):
-        return len(self.data)
+        return len(self.texts)
 
     def __getitem__(self, idx):
-        item = self.data[idx]
-        encoded_item = self.tokenizer.encode(item)
-        return encoded_item
+        text = self.texts[idx]
+        label = self.labels[idx]
+        encoding = self.tokenizer(text, return_tensors='pt', truncation=True, padding='max_length', max_length=10)
+        encoding['labels'] = torch.tensor(label, dtype=torch.long)
+        return encoding
 
-# Load the dataset
-train_dataset = TokenizedDataset("/cow02/rudenko/colowils/LLMExp/LLM-Exp/tokenizers/tokenized_data/hchem.pt", tokenizer)
+tokenizer = AutoTokenizer.from_pretrained('llama path')
+model = AutoModel.from_pretrained('llama path')
 
-# Create a DataLoader
-train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+train_dataset = MinimalDataset(tokenizer)
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False)
 
-# Load the pre-trained model (modify as needed for your specific model)
-model = AutoModel.from_pretrained("/cow02/rudenko/colowils/LLMExp/Llama-2-7b-chat-hf")
-if torch.cuda.device_count() > 1:
-    model = nn.DataParallel(model)
-model.to(device)
-
-# Define the training arguments
 training_args = TrainingArguments(
-    output_dir="./output",
+    output_dir='./output',
     overwrite_output_dir=True,
     num_train_epochs=1,
-    per_device_train_batch_size=16,
-    save_steps=10_000,
-    save_total_limit=2,
-    report_to="wandb",
-    learning_rate=0.01
+    per_device_train_batch_size=1,
+    logging_dir='./logs',
+    logging_steps=1,
+    save_total_limit=1,
 )
 
-# Define the Trainer
 trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=train_dataset,
 )
 
-# Begin training
 trainer.train()
-
-# Save the model
-trainer.save_model("./output")
 
 print("Training complete. Model saved to ./output")
